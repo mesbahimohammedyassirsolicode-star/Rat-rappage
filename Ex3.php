064e7961049424591faae1a8cb65ee0b77b64e42
ranya-tech <amzghar.ranya.solicode@gmail.com>
2026-03-09 17:41:27 +0100

    Revesion

---
+++ Ex3.php
@@ -0,0 +1,6 @@
+<?php
+$numbre = 5;
+for ($i = 1; $i <= 10; $i++) {
+    echo $numbre. "x" .$i ."= " . $numbre * $i . "<br>";
+}
+?>
---
+++ Ex5.php
@@ -0,0 +1,7 @@
+<?php
+$names = ["Ali", "Sara", "Omar", "Lina"];
+
+foreach ($names as $name) {
+    echo "Bonjour " . $name . "<br>";
+}
+?>
